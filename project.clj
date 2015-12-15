6e593649550e78fcae49438c5e56a1c4caefff63
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-15 14:07:16 +0800

    add day 01
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject advent "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solutions to the Advent of Code"
+  :url "https://github.com/coldnew/advent-of-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[michaelblume/lein-marginalia "0.9.0"]])
