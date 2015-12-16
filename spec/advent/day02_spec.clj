3dddf853bddd5b21035c428e3cb2885ad34df9ec
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-16 10:14:30 +0800

    day02 part2
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- spec/advent/day02_spec.clj
+++ spec/advent/day02_spec.clj
@@ -3,12 +3,20 @@
             [speclj.run.standard]
             [advent.day02 :refer :all]))
 
-(defn verify [test result]
+(defn verify-part1 [test result]
   (it (str test " is " result)
-      (should (= result (solve test)))))
+      (should (= result (solve-part1 test)))))
 
 (describe
- "Testcase in examples: day02"
- (verify "2x3x4"  58)
- (verify "1x1x10" 43)
- )
+ "Testcase in examples: day02 part-1"
+ (verify-part1 "2x3x4"  58)
+ (verify-part1 "1x1x10" 43))
+
+(defn verify-part2 [test result]
+  (it (str test " is " result)
+      (should (= result (solve-part2 test)))))
+
+(describe
+ "Testcase in examples: day02 part-2"
+ (verify-part2 "2x3x4"  34)
+ (verify-part2 "1x1x10" 14))
--- src/advent/day02.clj
+++ src/advent/day02.clj
@@ -38,10 +38,46 @@
     (+ (* 2 l w) (* 2 w h) (* 2 h l) k)))
 
 ;; solve all problem, problem must separate by '\n'
-(defn solve [input]
+(defn solve-part1 [input]
   (->>
    (map solve-1 (clojure.string/split input #"\n"))
    (reduce +)))
 
-;; solve this problem
-(solve data)
+;; part-1
+(solve-part1 data)
+
+;; --- Part Two ---
+
+;; The elves are also running low on ribbon. Ribbon is all the same width, so
+;; they only have to worry about the length they need to order, which they would
+;; again like to be exact.
+
+;; The ribbon required to wrap a present is the shortest distance around its
+;; sides, or the smallest perimeter of any one face. Each present also requires
+;; a bow made out of ribbon as well; the feet of ribbon required for the perfect
+;; bow is equal to the cubic feet of volume of the present. Don't ask how they
+;; tie the bow, though; they'll never tell.
+
+;; For example:
+
+;; A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap
+;; the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34
+;; feet.
+;; A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap
+;; the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14
+;; feet.
+
+;; How many total feet of ribbon should they order?
+
+(defn solve-2 [input]
+  (let [[l w h] (map #(Integer/parseInt %)
+                     (clojure.string/split input #"x"))
+        [m1 m2] (sort [l w h])]
+    (+ (+ m1 m1 m2 m2) (* l w h))))
+
+(defn solve-part2 [input]
+  (->> (map solve-2 (clojure.string/split input #"\n"))
+       (reduce +)))
+
+;; part-2
+(solve-part2 data)
