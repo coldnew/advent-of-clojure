7ac36418650520fecc8459abf11f875920db8623
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-16 12:30:54 +0800

    day3 part2
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- spec/advent/day03_spec.clj
+++ spec/advent/day03_spec.clj
@@ -12,3 +12,13 @@
  (verify-part1 ">"          2)
  (verify-part1 "^>v<"       4)
  (verify-part1 "^v^v^v^v^v" 2))
+
+(defn verify-part2 [test result]
+  (it (str test " is " result)
+      (should (= result (solve-part2 test)))))
+
+(describe
+ "Testcase in examples: day03 part2"
+ (verify-part2 "^v"          3)
+ (verify-part2 "^>v<"        3)
+ (verify-part2 "^v^v^v^v^v" 11))
