9995347c90210829090d645fd6e297d61fdfb9ca
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-12-18 22:37:41 +0800

    bump to clojure 1.8.0-rc4
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/coldnew/advent-of-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]]
   :test-paths ["spec"]
   :profiles {:dev {:dependencies [[speclj "3.3.0"]]}}
   :plugins [[speclj "3.3.0"]
